6a08502fec28d46c06374ff3db256dd1b44b974c
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-01-21 18:00:21 +0300

    adjust for natural numbers

--- testChart/chartTableViewCell.swift
+++ testChart/chartTableViewCell.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import Charts
 
 class chartTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var maxValueLabel: UILabel!
     @IBOutlet weak var frameView: UIView! { didSet{
         frameView.layer.cornerRadius = 16
         frameView.layer.borderWidth = 1
@@ -37,20 +39,44 @@ class chartTableViewCell: UITableViewCell {
     }
     
     func initView(newData: [CGFloat]) {
-        let maxValue = newData.reduce(CGFloat.leastNormalMagnitude) { max($0, $1) }
+        let biggestNumber = newData.reduce(CGFloat.leastNormalMagnitude) { max($0, $1) }
+        let maxValue = round( biggestNumber / 100 ) * 100
+        let minValue = round( newData.reduce(CGFloat.greatestFiniteMagnitude) { min($0, $1) } / 100 ) * 100
+        maxValueLabel.text = String(format: "%.0F", biggestNumber)
         self.data = newData.map{$0 / maxValue * 100}
-        populateLabels(maxValue)
+        populateLabels(maxValue: maxValue, minValue: minValue, newData: newData)
         zigChart.data = self.data
     }
     
-    private func populateLabels(_ maxValue: CGFloat) {
+    private func populateLabels(maxValue: CGFloat, minValue: CGFloat, newData: [CGFloat]) {
         guard !data.isEmpty else { return }
-        label1.text = "\(maxValue)"
-        label2.text = "\(round(maxValue * 0.75))"
-        label3.text = "\(round(maxValue * 0.25))"
-        label4.text = "0"
+        var unitValue = (maxValue - minValue) / 3
+        
+        var multiplyer: CGFloat = 1
+        while unitValue > 10 {
+            unitValue /= 10
+            multiplyer *= 10
+        }
+        guard unitValue == round(unitValue) else {
+            let adjustedData = newData.map{$0/round(unitValue) * unitValue }
+            let biggestAdjustedNumber = newData.reduce(CGFloat.leastNormalMagnitude) { max($0, $1) }
+            let maxAdjustedValue = round( biggestAdjustedNumber / 100 ) * 100
+            self.data = adjustedData.map{$0 / maxAdjustedValue * 100}
+            let baselineNumber = round(unitValue) * 3
+            let minValue = round( newData.reduce(CGFloat.greatestFiniteMagnitude) { min($0, $1) } / 100 ) * 100
+            label1.text = String(format: "%.0F", round(baselineNumber * multiplyer))
+            label2.text = String(format: "%.0F", round(baselineNumber / 3 * 2 * multiplyer))
+            label3.text = String(format: "%.0F", round(baselineNumber / 3  * multiplyer))
+            label4.text = "\(Int(minValue))"
+            
+            return
+        }
+        label1.text = "\(Int(maxValue))"
+        label2.text = String(format: "%.0F", round(unitValue * 2 * multiplyer))
+        label3.text = String(format: "%.0F", round(unitValue * multiplyer))
+        label4.text = "\(Int(minValue))"
     }
-    
+
     private func addStroakLines() {
         stroakLine1.addStroakLine(lineYPosition: 0, lineWidth: 1, color: UIColor(hex: "#ECEDF1"), dashPattern: [4,4], viewWidth: strokeLineWidth)
         stroakLine2.addStroakLine(lineYPosition: 0, lineWidth: 1, color: UIColor(hex: "#ECEDF1"), dashPattern: [4,4], viewWidth: strokeLineWidth)
